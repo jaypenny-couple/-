3e419f07c403f0e671a24512e69504fd4c65eab5
jaypenny-couple <aa316112@gmail.com>
2026-06-30 14:21:30 +0800

    Set active navigation state automatically

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const navToggle = document.querySelector(".nav-toggle");
 const siteNav = document.querySelector(".site-nav");
 
+const getPageName = (pathname) => {
+  const cleanPath = pathname.replace(/\/+$/, "");
+  const lastSegment = cleanPath.split("/").pop() || "index.html";
+  return lastSegment === "-" ? "index.html" : lastSegment;
+};
+
+const currentPage = getPageName(window.location.pathname);
+
+if (siteNav) {
+  siteNav.querySelectorAll("a").forEach((link) => {
+    try {
+      const linkUrl = new URL(link.getAttribute("href"), window.location.href);
+      const linkPage = getPageName(linkUrl.pathname);
+      const isSameOrigin = linkUrl.origin === window.location.origin;
+
+      if (isSameOrigin && linkPage === currentPage) {
+        link.setAttribute("aria-current", "page");
+      } else {
+        link.removeAttribute("aria-current");
+      }
+    } catch (error) {
+      link.removeAttribute("aria-current");
+    }
+  });
+}
+
 if (navToggle && siteNav) {
   navToggle.addEventListener("click", () => {
     const isOpen = navToggle.getAttribute("aria-expanded") === "true";
